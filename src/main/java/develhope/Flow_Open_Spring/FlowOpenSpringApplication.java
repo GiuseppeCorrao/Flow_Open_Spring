e52f25342a04aa08850d6921b675bfdd51da7df9
Giuseppe Corrao <josephcorr99@gmail.com>
2022-10-12 17:48:08 +0200

    initial commit

---
+++ src/main/java/develhope/Flow_Open_Spring/FlowOpenSpringApplication.java
@@ -0,0 +1,13 @@
+package develhope.Flow_Open_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowOpenSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowOpenSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/develhope/Flow_Open_Spring/FlowOpenSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package develhope.Flow_Open_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowOpenSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
