7576a778973981013a11e94390bf7a68033ca5b7
Mirko Di Cristina <mirko7726@gmail.com>
2022-10-20 11:00:05 +0200

    Fix: added a controller CRUD

--- src/main/java/develhope/Flow_Open_Spring/controller/ProductController.java
+++ src/main/java/develhope/Flow_Open_Spring/controller/ProductController.java
@@ -1,9 +1,47 @@
 package develhope.Flow_Open_Spring.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import develhope.Flow_Open_Spring.entities.Product;
+import develhope.Flow_Open_Spring.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @PostMapping(value = "/create")
+    public Product createProduct(@RequestBody Product product){
+        return productRepository.save(product);
+    }
+
+    @GetMapping(value = "")
+    public List<Product> getProduct(){
+        return productRepository.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Optional<Product> getOneProduct(@PathVariable Long id){
+        return productRepository.findById(id);
+    }
+
+    @PutMapping(value = "/edit")
+    public Product updateProduct(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+    @DeleteMapping(value = "/{id}")
+    public HttpStatus deleteProduct(@PathVariable Long id){
+        if(productRepository.existsById(id)){
+            productRepository.deleteById(id);
+            return HttpStatus.ACCEPTED;
+        } else {
+            return HttpStatus.NO_CONTENT;
+        }
+    }
 }
