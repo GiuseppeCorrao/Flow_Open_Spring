35ee3f2f61a57808b7b0cdf54197f869d835d8f6
Giuseppe Corrao <josephcorr99@gmail.com>
2022-10-22 16:07:29 +0200

    refactor: change environment variables on file .yml

--- src/main/java/develhope/Flow_Open_Spring/repositories/UserRepository.java
+++ src/main/java/develhope/Flow_Open_Spring/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package develhope.Flow_Open_Spring.repositories;
 
 import develhope.Flow_Open_Spring.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
