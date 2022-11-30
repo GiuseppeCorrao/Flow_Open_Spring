9e6cefcf2233adb1b4ea5a9df739b02ac4b06de0
Emmanuele La Duca <laducaemanuele20@gmail.com>
2022-11-30 21:44:45 +0100

    add function file upload and download

---
+++ src/main/java/develhope/Flow_Open_Spring/entities/Orders.java
@@ -0,0 +1,109 @@
+package develhope.Flow_Open_Spring.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class Orders {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(unique = true)
+    private long id;
+
+    @Column(nullable = false)
+    private String color;
+
+    @Column(nullable = false)
+    private double price;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(nullable = false)
+    private double priceDelivery;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Brand brand;
+
+    public Orders(long id, String color, double price, String description, String name, int quantity, double priceDelivery, Brand brand) {
+        this.id = id;
+        this.color = color;
+        this.price = price;
+        this.description = description;
+        this.name = name;
+        this.quantity = quantity;
+        this.priceDelivery = priceDelivery;
+        this.brand = brand;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPriceDelivery() {
+        return priceDelivery;
+    }
+
+    public void setPriceDelivery(double priceDelivery) {
+        this.priceDelivery = priceDelivery;
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
+}
